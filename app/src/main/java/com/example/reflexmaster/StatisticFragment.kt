8282c6a61940c65bd32aa849ffa628a13ffeaf1f
Peter Hodás <peterhodas3@gmail.com>
2023-04-28 10:58:25 +0200

    Preklikavanie fragmentov

--- app/src/main/java/com/example/reflexmaster/StatisticFragment.kt
+++ app/src/main/java/com/example/reflexmaster/StatisticFragment.kt
@@ -5,10 +5,9 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
-import androidx.navigation.findNavController
 
-class MenuFragment : Fragment() {
+
+class StatisticFragment : Fragment() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,17 +21,6 @@ class MenuFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_menu, container, false)
+        return inflater.inflate(R.layout.fragment_statistic, container, false)
     }
-
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-
-        val btnvys: Button = view.findViewById(R.id.btn_vysledok)
-
-        btnvys.setOnClickListener {
-            view.findNavController().navigate(R.id.action_menuFragment_to_statisticFragment)
-        }
-    }
-
 }
